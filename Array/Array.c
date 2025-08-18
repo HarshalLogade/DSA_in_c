62b9e5236db2cb9970fc092d5836106e4f8dadd5
Harshal Logade <harshal87logade@gmail.com>
2025-08-18 22:42:37 +0530

    18/8/25

---
+++ Array/Array.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct myArray
+{
+    int total_size;
+    int used_size;
+    int *ptr;
+};
+
+void createArray(struct myArray * a, int tsize, int uSize){
+    //a->total_size = tsize;
+
+    (*a).total_size = tsize;
+    (*a).used_size = uSize;
+    (*a).ptr = (int *)malloc(tsize * sizeof(int));
+}
+
+void showArray(struct myArray * a){
+    for (int i = 0; i < a->used_size; i++)
+    {
+        printf("%d\n", (a->ptr)[i]);
+    }
+    
+}
+
+void setArray(struct myArray * a){
+    int n;
+    for (int i = 0; i < a->used_size; i++)
+    {
+        printf("%d = ", i);
+        scanf("%d", &n);
+        (a->ptr)[i] = n;
+    }
+    
+}
+
+int main(){
+    struct myArray arr;
+    createArray(&arr, 10, 2);
+    printf("Enter elements: \n");
+    setArray(&arr);
+    printf("Show elements: \n");
+    showArray(&arr);
+    return 0;
+}
